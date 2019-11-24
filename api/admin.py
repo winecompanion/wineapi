dac4bfe23b0ee0fc9847426f99b396b8e35aee58
juan <eliasjuanpablo1@gmail.com>
2019-11-24 20:37:16 -0300

    Register all resources

--- api/admin.py
+++ api/admin.py
@@ -2,16 +2,55 @@ from django.contrib import admin
 
 from api.models import (
     Country,
+    Event,
+    EventOccurrence,
     EventCategory,
     Tag,
     Language,
     Gender,
+    Rate,
+    Reservation,
     Varietal,
+    Wine,
+    WineLine,
+    Winery,
 )
 
-admin.site.register(Country)
+
+class WineryAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+class EventAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'winery__name')
+
+
+class EventOccurrenceAdmin(admin.ModelAdmin):
+    search_fields = ('event__name', 'start')
+
+
+class ReservationAdmin(admin.ModelAdmin):
+    search_fields = ('user__email', 'event_occurrence__event__name', 'user__last_name', 'user__first_name')
+
+
+class CountryAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+
+
+class RateAdmin(admin.ModelAdmin):
+    search_fields = ('created', 'user__email', 'user__last_name', 'user__first_name')
+
+
+admin.site.register(Country, CountryAdmin)
 admin.site.register(Language)
 admin.site.register(Gender)
 admin.site.register(Varietal)
 admin.site.register(EventCategory)
 admin.site.register(Tag)
+admin.site.register(Winery, WineryAdmin)
+admin.site.register(WineLine)
+admin.site.register(Wine)
+admin.site.register(Event, EventAdmin)
+admin.site.register(EventOccurrence, EventOccurrenceAdmin)
+admin.site.register(Rate, RateAdmin)
+admin.site.register(Reservation, ReservationAdmin)
