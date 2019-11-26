ccb52e167fbc37a4346788f7159d657fe073b9be
Juan Pablo Vallejo <juampivallejo97@gmail.com>
2019-11-26 08:03:35 -0300

    fix password setting

--- users/models.py
+++ users/models.py
@@ -26,7 +26,7 @@ class WineUserManager(BaseUserManager):
     Custom user model manager where email is the unique identifiers
     for authentication instead of usernames.
     """
-    def create_user(self, email, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """
         Create and save a User with the given email and password.
         """
@@ -34,7 +34,10 @@ class WineUserManager(BaseUserManager):
             raise ValueError('The Email must be set')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
-        user.set_password(''.join(random.choices(string.ascii_uppercase + string.digits, k=20)))
+
+        if not password:
+            password = ''.join(random.choices(string.ascii_uppercase + string.digits, k=20))
+        user.set_password(password)
         user.save()
         return user
 
@@ -58,7 +61,7 @@ class WineUserManager(BaseUserManager):
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
-        return self.create_user(email, **extra_fields)
+        return self.create_user(email, password, **extra_fields)
 
 
 class WineUser(AbstractBaseUser, PermissionsMixin):
